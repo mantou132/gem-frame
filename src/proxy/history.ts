ec8c1469be0ab9aa6f3d81fcca93eb9a293c361f
mantou132 <709922234@qq.com>
2020-03-24 01:18:10 +0800

    Allow external routing
    Fixed #13

--- src/proxy/history.ts
+++ src/proxy/history.ts
@@ -2,7 +2,16 @@ import { generateProxy, getAbsolutePath } from '../utils';
 
 import GemFrame from '../index';
 
+const ROOT = '/~root/';
+
 export function getHistory(frameElement: GemFrame) {
+  function getPath(path: string) {
+    if (path.startsWith(ROOT)) {
+      return path.replace(ROOT, '/');
+    } else {
+      return getAbsolutePath(path, frameElement.basepath);
+    }
+  }
   const taget = {
     get length() {
       return history.length;
@@ -24,10 +33,10 @@ export function getHistory(frameElement: GemFrame) {
       history.forward();
     },
     pushState: (data: any, title: string, path: string) => {
-      history.pushState(data, title, getAbsolutePath(path, frameElement.basepath));
+      history.pushState(data, title, getPath(path));
     },
     replaceState: (data: any, title: string, path: string) => {
-      history.replaceState(data, title, getAbsolutePath(path, frameElement.basepath));
+      history.replaceState(data, title, getPath(path));
     },
   };
 
